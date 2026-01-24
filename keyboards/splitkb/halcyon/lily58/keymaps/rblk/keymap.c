f2800ed647b62270ade07ac2e287b85c7fc9664c
Rüdiger Block <ruediger.block@gmx.de>
2026-01-24 17:39:27 +0100

    feat: implement os differences using layers

--- keyboards/splitkb/halcyon/lily58/keymaps/rblk/keymap.c
+++ keyboards/splitkb/halcyon/lily58/keymaps/rblk/keymap.c
@@ -9,63 +9,73 @@
 #include "config.h"
 #include "keymap_german.h"
 
-
 void keyboard_post_init_user(void) {
     rgb_matrix_sethsv(255, 255, 255);
 }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    // DEFAULT
-
     [0] = LAYOUT_lily58_hlc(
-    //      ESC         1           2           3           4           5                                       6           7           8           9           0           BSPC
-            KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                                   KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_BSPC,
-    //      TAB         Q           W           E           R           T                                       Z           U           I           O           P           +
-            KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                                   KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       DE_PLUS,
-    //                  A           S           D           F           G                                       H           J           K           L           #           SFT
-            KC_LSFT,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                                   KC_H,       KC_J,       KC_K,       KC_L,       DE_HASH,    KC_RSFT,
-    //      SFT         Y           X           C           V           B                                       N           M           ,           .           -           SPC
-            KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       XXXXXXX,        XXXXXXX,    KC_N,       KC_M,       KC_COMM,    KC_DOT,     DE_MINS,    KC_RSFT,
-    //                                          LCTL        OPT         CMD / ALT   SPC | L(1)      SPC         ALTGR       CMD         RCTL
-                                                KC_LCTL,    RB_LCMD2,   RB_LCMD1,   LT(1, KC_SPC),  KC_SPC,     RB_RCMD1,   RB_RCMD2,   KC_RCTL,
 
-                                                KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
+            KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                                   KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_BSPC,
+            KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                                   KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       DE_PLUS,
+            KC_LSFT,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                                   KC_H,       KC_J,       KC_K,       KC_L,       DE_HASH,    KC_RSFT,
+            KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       MO(3),          MO(4),      KC_N,       KC_M,       KC_COMM,    KC_DOT,     DE_MINS,    KC_RSFT,
+                                                KC_LCTL,    KC_LGUI,    KC_LALT,    LT(2, KC_SPC),  KC_SPC,     KC_RALT,    KC_RGUI,    KC_RCTL,
+
+            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
 
     [1] = LAYOUT_lily58_hlc(
-    //      ^                                                                                                                                   ß           ´           DEL
-            DE_GRV,     _______,    _______,    _______,    _______,    _______,                            _______,    _______,    _______,    DE_SS,      DE_ACUT,    KC_DELETE,
-    //                                                                                                                  Ü                       Ö
-            _______,    _______,    _______,    _______,    _______,    _______,                            _______,    DE_UDIA,    _______,    DE_ODIA,    _______,    _______,
-    //                  Ä                                                                                   <-          v           ^           ->
-            _______,    DE_ADIA,    _______,    _______,    _______,    _______,                            KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   _______,    _______,
-    //                  <                                                                                   1           2           3           4           5
-            _______,    DE_LABK,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,     _______,   _______,    _______,
-    //                                                                                          ENT
-                                                _______,    _______,    _______,    _______,    KC_ENTER,   _______,    _______,    _______,
 
-                                                _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,     _______,    _______),
-
-    [12] = LAYOUT_lily58_hlc(
             _______,    _______,    _______,    _______,    _______,    _______,                            _______,    _______,    _______,    _______,    _______,    _______,
             _______,    _______,    _______,    _______,    _______,    _______,                            _______,    _______,    _______,    _______,    _______,    _______,
             _______,    _______,    _______,    _______,    _______,    _______,                            _______,    _______,    _______,    _______,    _______,    _______,
             _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,
-                                                _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,
+                                                _______,    KC_LALT,    KC_LGUI,    _______,    _______,    KC_RGUI,    KC_RALT,    _______,
 
-                                                _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______),
+            _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______),
+
+    [2] = LAYOUT_lily58_hlc(
+
+            DE_CIRC,    _______,    _______,    _______,    _______,    _______,                            _______,    _______,    _______,    DE_SS,      DE_ACUT,    KC_DEL,
+            _______,    _______,    _______,    _______,    _______,    _______,                            _______,    DE_UDIA,    _______,    DE_ODIA,    _______,    _______,
+            _______,    DE_ADIA,    _______,    _______,    _______,    _______,                            KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   _______,    KC_ENT,
+            KC_LCTL,    DE_LABK,    _______,    _______,    _______,    _______,    KC_PGUP,    KC_PGDN,    _______,    _______,    _______,    _______,    _______,    KC_RCTL,
+                                                _______,    _______,    _______,    _______,    KC_ENT,     _______,    _______,    _______,
+
+            _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______),
+
+    [3] = LAYOUT_lily58_hlc(
+
+            _______,    _______,    _______,    _______,    _______,    _______,                            _______,    _______,    _______,    _______,    _______,    KC_BRIU,
+            _______,    _______,    _______,    _______,    _______,    _______,                            KC_7,       KC_8,       KC_9,       DE_MINS,    _______,    KC_BRID,
+            _______,    _______,    _______,    _______,    _______,    _______,                            KC_4,       KC_5,       KC_6,       DE_PLUS,    _______,    KC_VOLU,
+            _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_1,       KC_2,       KC_3,       KC_ENTER,   _______,    KC_VOLD,
+                                                _______,    _______,    _______,    _______,    _______,    KC_0,       KC_DOT,     _______,
+
+            _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______),
+
+    [4] = LAYOUT_lily58_hlc(
+
+            KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,                              _______,    _______,    _______,    _______,    _______,    _______,
+            KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,                             _______,    _______,    _______,    _______,    _______,    _______,
+            _______,    _______,    _______,    _______,    _______,    _______,                            _______,    _______,    _______,    _______,    _______,    _______,
+            _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,
+                                                _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,
+
+            _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______),
+
+    [12] = LAYOUT_lily58_hlc(
+
+            _______,    _______,    _______,    _______,    _______,    _______,                            _______,    _______,    _______,    _______,    _______,    _______,
+            _______,    _______,    _______,    _______,    _______,    _______,                            _______,    _______,    _______,    _______,    _______,    _______,
+            _______,    _______,    _______,    _______,    _______,    _______,                            _______,    _______,    _______,    _______,    _______,    _______,
+            _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,
+                                                _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,
+
+            _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______),
 
 };
-#if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_mapkk[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_PGUP, KC_PGDN)},
-    [1] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_PGUP, KC_PGDN)},
-    [2] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_PGUP, KC_PGDN)},
-    [3] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_PGUP, KC_PGDN), ENCODER_CCW_CW(KC_PGUP, KC_PGDN)}
-};
-#endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
-
-
 
 #ifdef OTHER_KEYMAP_C
 #    include OTHER_KEYMAP_C
--- keyboards/splitkb/halcyon/lily58/keymaps/rblk/os.h
+++ keyboards/splitkb/halcyon/lily58/keymaps/rblk/os.h
@@ -1,54 +1,27 @@
-
-enum my_keycodes {
-  RB_RCMD1 = SAFE_RANGE,
-  RB_RCMD2,
-  RB_LCMD1,
-  RB_LCMD2,
-};
-
 bool process_detected_host_os_kb(os_variant_t detected_os) {
     if (!process_detected_host_os_user(detected_os)) {
         return false;
     }
+
     switch (detected_os) {
         case OS_WINDOWS:
             // nein
+            break;
         case OS_MACOS:
+            rgb_matrix_sethsv(HSV_BLUE);
+            layer_on(1);
+            break;
         case OS_IOS:
-            rgb_matrix_sethsv(0, 255, 0);
-            #ifndef RB_RCMD1
-                #define RB_RCMD1 KC_RGUI
-            #endif
-            #ifndef RB_RCMD2
-                #define RB_RCMD2 KC_RALT
-            #endif
-            #ifndef RB_LCMD1
-                #define RB_LCMD1 KC_LGUI
-            #endif
-            #ifndef RB_LCMD2
-                #define RB_LCMD2 KC_LALT
-            #endif
+            rgb_matrix_sethsv(HSV_BLUE);
+            layer_on(1);
             break;
         case OS_LINUX:
-            rgb_matrix_sethsv(0, 0, 255);
-            #ifndef RB_RCMD1
-                #define RB_RCMD1 KC_RALT
-            #endif
-            #ifndef RB_RCMD2
-                #define RB_RCMD2 KC_RGUI
-            #endif
-            #ifndef RB_LCMD1
-                #define RB_LCMD1 KC_LALT
-            #endif
-            #ifndef RB_LCMD2
-                #define RB_LCMD2 KC_LGUI
-            #endif
+            rgb_matrix_sethsv(15, 255, 100);
             break;
         case OS_UNSURE:
-            rgb_matrix_sethsv(255, 0, 0);
+            rgb_matrix_sethsv(HSV_RED);
             break;
     }
 
     return true;
 }
-
