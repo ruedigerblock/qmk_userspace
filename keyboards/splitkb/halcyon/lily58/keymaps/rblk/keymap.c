6d8f312d37dca648b246faa9340ac9c3a21c2b09
Rüdiger Block <ruediger.block@gmx.de>
2026-02-20 17:00:14 +0100

    feat: add symbol layer

--- keyboards/splitkb/halcyon/lily58/keymaps/rblk/keymap.c
+++ keyboards/splitkb/halcyon/lily58/keymaps/rblk/keymap.c
@@ -9,6 +9,8 @@
 #include "os.h"
 #include "config.h"
 
+#define LEADER_TIMEOUT 400
+
 void keyboard_post_init_user(void) {
     rgb_matrix_sethsv(255, 255, 255);
 }
@@ -17,11 +19,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_lily58_hlc(
 
-            KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                                       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_BSPC,
-            KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                                       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       DE_PLUS,
-            KC_LSFT,    KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                                       KC_H,       KC_J,       KC_K,       KC_L,       DE_HASH,    KC_RSFT,
-            KC_LCTL,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       MO(2),          MO(3),          KC_N,       KC_M,       KC_COMM,    KC_DOT,     DE_MINS,    KC_RCTL,
-                                                KC_LCTL,    KC_LGUI,    KC_LALT,    LT(1, KC_SPC),  LT(4, KC_SPC),  KC_RALT,    KC_RGUI,    KC_RCTL,
+            LT(5, KC_ESC),  KC_1,       KC_2,       KC_3,       KC_4,       KC_5,                                       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_BSPC,
+            KC_TAB,         KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,                                       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       DE_PLUS,
+            KC_LSFT,        KC_A,       KC_S,       KC_D,       KC_F,       KC_G,                                       KC_H,       KC_J,       KC_K,       KC_L,       DE_HASH,    KC_RSFT,
+            KC_LCTL,        KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       MO(2),          MO(3),          KC_N,       KC_M,       KC_COMM,    KC_DOT,     DE_MINS,    QK_LEAD,
+                                                    KC_LCTL,    KC_LGUI,    KC_LALT,    LT(1, KC_SPC),  LT(4, KC_SPC),  KC_RALT,    KC_RGUI,    KC_RCTL,
 
             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
 
@@ -62,6 +64,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______,    DE_PIPE,    DE_LABK,    DE_LBRC,    DE_LCBR,    S(DE_8),                            S(DE_9),    DE_RCBR,    DE_RBRC,    S(DE_LABK), _______,    _______,
             _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,
                                                 _______,    _______,    _______,    KC_SPC,     _______,    _______,    _______,    _______,
+
+            _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______),
+
+    [5] = LAYOUT_lily58_hlc(
+
+            _______,    _______,    _______,    _______,    _______,    _______,                            _______,    _______,    _______,    _______,    DE_ACUT,    KC_DEL,
+            _______,    _______,    _______,    _______,    _______,    _______,                            _______,    DE_UDIA,    _______,    DE_ODIA,    _______,    DE_MINS,
+            _______,    DE_ADIA,    DE_SS,      _______,    _______,    _______,                            _______,    _______,    _______,    _______,    _______,    _______,
+            _______,    DE_LABK,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,
+                                                _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,
 
             _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______),
 
